3a36dddaf37fdecbf74372db3c6cb12d7a6d6617
anthonyqsun <qsun30@stuy.edu>
2021-11-24 12:22:04 -0500

    finalize v2

---
+++ Troll.java
@@ -0,0 +1,10 @@
+public class Troll extends Monster {
+  public Troll(){
+    super();
+    health = 35;
+    defense = 6;
+  }
+  public static String about(){
+    return "This beast ate one too many taco bell meals. It can take a beating on both the toilet and the battlefield.";
+  }
+}
