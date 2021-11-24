a5917b2d9453ee14323b770635f9b9b84ad684b3
anthonyqsun <qsun30@stuy.edu>
2021-11-24 10:30:58 -0500

    retrack files

--- v1/Protagonist.java
+++
@@ -1,7 +0,0 @@
-public class Protagonist extends Character {
-  public Protagonist() {
-    health = 50;
-    strength = 10;
-    defense = 5;
-  }
-}
--- v2/Goblin.java
+++
@@ -1,9 +0,0 @@
-public class Goblin extends Monster {
-  public Goblin(){
-    super();
-    attackRating += (Math.random() / 2);
-  }
-  public static String about(){
-    return "This little brat is quite weak, but it has a random chance to hit a little harder.";
-  }
-}
